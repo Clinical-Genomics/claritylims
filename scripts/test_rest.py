0d3c7d83dfb054a024f8f99d8f692bc4d633e3e9
Rikard <rikard.erlandsson@scilifelab.se>
2014-11-24 14:18:53 +0100

    Create test_rest.py

---
+++ scripts/test_rest.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+#Script that connects to the MySQL database and parses data from an html table
+#Import the mysql.connector library/module
+import sys
+import time
+import glob
+import re
+import os
+import requests
+
+
+print hello
