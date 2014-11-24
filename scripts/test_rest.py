6ccd911abd18029eb12dab90e038f97e15170e24
Rikard <rikard.erlandsson@scilifelab.se>
2014-11-24 15:15:53 +0100

    Update test_rest.py

--- scripts/test_rest.py
+++ scripts/test_rest.py
@@ -16,12 +16,16 @@ user1 = "apiuser"
 pass1 = "rushverbpureking"
 
 r = requests.get(baseurl, auth=(user1, pass1))
-print r.status_code
-print r.headers['content-type']
-print r.encoding
-print r.text
+#print r.status_code
+#print r.headers['content-type']
+#print r.encoding
+#print r.text
 
 tree = ET.ElementTree(ET.fromstring(r.text))
-print tree.getroot()
+root = tree.getroot()
+
+for child in root:
+  print child.tag, child.attribute
+  
 
 exit
