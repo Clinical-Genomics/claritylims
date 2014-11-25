054ee6a8008006d2bfea067e266c7176312831d3
Rikard <rikard.erlandsson@scilifelab.se>
2014-11-25 08:52:36 +0100

    Update test_rest.py

--- scripts/test_rest.py
+++ scripts/test_rest.py
@@ -51,6 +51,7 @@ for sample in rsmpl:
   print '  in limsid:', sample.attrib['limsid']
   singlev = requests.get(sample.attrib['uri'], auth=(user1, pass1))
 #  svt = ET.ElementTree(ET.fromstring(singlev.text))
-  print singlev.text, singlev.keys() #svt.attrib['name']
+  svt = ET.Element(ET.fromstring(singlev.text))
+  print svt.keys() #svt.attrib['name']
   
 exit
