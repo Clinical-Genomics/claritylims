9430a70e8f8e2c4926e9debbdf010b0772609efa
Rikard <rikard.erlandsson@scilifelab.se>
2014-11-25 13:19:18 +0100

    Update limsid2sampleid.py

--- scripts/limsid2sampleid.py
+++ scripts/limsid2sampleid.py
@@ -19,6 +19,8 @@ if (len(sys.argv)>2):
 if (len(sys.argv) == 1):
   print "\n\tusage: $0 <LIMSID> <config-file:optional>\n"
   exit(0)
+else:
+  search = sys.argv[1]
     
 params = {}
 with open(configfile, "r") as confs:
@@ -61,16 +63,18 @@ while mybrain == 'empty':
   if previous == URL:
     for sample in rsmpl:
       if sample.tag == "sample":
-        LIMSID = sample.attrib['limsid']
-        singlev = requests.get(sample.attrib['uri'], auth=(user1, pass1), 
-            headers={'content-type': 'application/xml', 'accept': 'application/xml'})
-        svt = ET.ElementTree(ET.fromstring(singlev.text.encode('utf-8')))
-        elem = svt.getroot()
-        for element in elem:
-          if element.tag == 'name':
-            SAMPLEID = element.text
-            COUNTER += 1
-        print str(COUNTER) + "     LIMSID " + LIMSID + "     SAMPLEID " + SAMPLEID 
+        if sample.attrib['limsid'] = search:
+          LIMSID = sample.attrib['limsid']
+          singlev = requests.get(sample.attrib['uri'], auth=(user1, pass1), 
+              headers={'content-type': 'application/xml', 'accept': 'application/xml'})
+          svt = ET.ElementTree(ET.fromstring(singlev.text.encode('utf-8')))
+          elem = svt.getroot()
+          for element in elem:
+            if element.tag == 'name':
+              SAMPLEID = element.text
+              COUNTER += 1
+          print str(COUNTER) + "     LIMSID " + LIMSID + "     SAMPLEID " + SAMPLEID 
+          mybrain = 'intoxicated'
       else:
         if sample.tag == "next-page":
           URL = sample.attrib['uri']
