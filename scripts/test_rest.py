8358c698a9838059b4123958c69cb1de69ac4b16
Rikard <rikard.erlandsson@scilifelab.se>
2015-01-22 14:02:04 +0100

    Update test_rest.py

--- scripts/test_rest.py
+++ scripts/test_rest.py
@@ -41,6 +41,7 @@ for node in tree.findall('sample'):
     internal_id = node.attrib['limsid']
     print internal_id
     suburi = node.attrib['uri']
+    print suburi
     rr = requests.get(suburi, auth=(user1, pass1))
     subtree = ElementTree.fromstring(rr.text)
     name = subtree.findall('name')
