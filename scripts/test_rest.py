35643b5a77fb7ccabff679c203502d87b91c2c1f
Rikard <rikard.erlandsson@scilifelab.se>
2015-01-23 08:44:23 +0100

    Update test_rest.py

--- scripts/test_rest.py
+++ scripts/test_rest.py
@@ -19,6 +19,8 @@ params = {}
 with open(configfile, "r") as confs:
   for line in confs:
     if len(line) > 5 and not line[0] == "#":
+#        line = line.
+      pv = line.rstrip().split(" ")
       arg = pv[0]
       pv.pop(0)
       params[arg] = ' '.join(pv)
