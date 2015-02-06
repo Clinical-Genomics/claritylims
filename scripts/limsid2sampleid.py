5398cca563898de9523050d3468525fc05ceb485
Rikard <rikard.erlandsson@scilifelab.se>
2015-02-06 11:22:20 +0100

    Update limsid2sampleid.py

--- scripts/limsid2sampleid.py
+++ scripts/limsid2sampleid.py
@@ -21,5 +21,12 @@ with limsconnect(pars['apiuser'], pars['apipass'], pars['baseuri']) as lmc:
     print node.tag
     for key in node.attrib:
       print key, node.attrib[key]
+      
+  hit = getentry('samples', 'SIB802A28')
+  for node in hit:
+    print node.tag
+    for key in node.attrib:
+      print key, node.attrib[key]
+  
 #  for entry in entries:
 #    print entry 
