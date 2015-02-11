3b401150d64a21a97a40d225782a8abf7d3ad08f
Rikard <rikard.erlandsson@scilifelab.se>
2015-02-11 13:25:34 +0100

    Create getname.py

---
+++ scripts/getname.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+#Script that connects to the MySQL database and parses data from an html table
+#Import the mysql.connector library/module
+import sys
+import time
+import glob
+import re
+import os
+from limsaccess import *
+
+if (len(sys.argv)>1):
+  configfile = sys.argv[1]
+else:
+  configfile = 'None'
+pars = readconfig(configfile)
+
+with limsconnect(pars['apiuser'], pars['apipass'], pars['baseuri']) as lmc:
+  
